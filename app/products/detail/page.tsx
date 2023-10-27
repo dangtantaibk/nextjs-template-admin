9e8af08fc48426953bbb3e325ef8497e0e68997b
Dương Anh Phụng <phung.da@kdc.vn>
2023-10-27 08:14:26 +0700

    update product

--- app/products/detail/page.tsx
+++ app/products/detail/page.tsx
@@ -6,7 +6,9 @@ import Buttons from "@/components/Buttons";
 import moment from "moment";
 import Loading from 'components/Loading';
 import Link from "next/link";
+import Notification from "@/components/Notification";
 
+import { AUTH_DOMAIN } from 'constant';
 import { useForm } from "react-hook-form";
 import { useRouter } from 'next/navigation';
 
@@ -38,6 +40,7 @@ const ProductDetailPage = () => {
   const router = useRouter();
   const [productDetail, setProductDetail] = useState<ProductsDetailProp>()
   const [loading, setloading] = useState(false);
+  const [urlFile, setUrlFile] = useState("");
   const {
     register,
     handleSubmit,
@@ -45,13 +48,12 @@ const ProductDetailPage = () => {
     formState: { errors },
   } = useForm<any>();
 
-  useEffect(() => {
-    if (productDetail) {
-      setValue("description", productDetail.description || '');
-      setValue("price", productDetail.price || 0);
-      setValue("stock", productDetail.stock || 0);
-    }
-  }, [productDetail]);
+  const notiDetail = {
+    isOpen: false,
+    message: "",
+    type: ""
+  }
+  const [notification, setNotification] = useState(notiDetail);
 
   const getProducts = async (id) => {
     setloading(true)
@@ -59,6 +61,10 @@ const ProductDetailPage = () => {
     try {
       if (resp.data) {
         setProductDetail(resp.data);
+        setValue("description", resp.data.description || '');
+        setValue("price", resp.data.price || 0);
+        setValue("stock", resp.data.stock || 0);
+        setUrlFile(resp.data.image)
       }
     } catch (error) {
       console.log(error)
@@ -81,7 +87,8 @@ const ProductDetailPage = () => {
       ...productDetail,
       price: Number(data.price),
       stock: Number(data.stock),
-      description: data.description
+      description: data.description,
+      image: urlFile,
     }
     const resp = await request.put(`api/v1/products/${id}`, params);
     try {
@@ -92,8 +99,48 @@ const ProductDetailPage = () => {
       setloading(false);
     }
   })
+
+  const uploadAvatarUser = async (file) => {
+    const TYPE_IMAGE = ['image/png', 'image/jpeg', 'image/gif'];
+    const formData = new FormData();
+    const token = await localStorage.getItem('auth');
+    if (!TYPE_IMAGE.includes(file.type)) {
+      setNotification({ isOpen: true, message: "Vui lòng chọn file ảnh *png, *jpeg, *gif", type: "error" })
+    } else {
+      formData.append('file', file);
+      await fetch(`https://${AUTH_DOMAIN[location.host]}api/v1/upload`, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          try {
+            if (data.data) {
+              setUrlFile(data.data.url)
+              setNotification({ isOpen: true, message: "Upload file thành công", type: "success" })
+            } else {
+              setNotification({ isOpen: true, message: data.message, type: "error" })
+            }
+          } catch (error) {
+            console.log(error)
+          }
+        })
+        .catch(error => {
+          console.error('Upload failed:', error);
+        });
+    }
+  }
+
   return (
     <form onSubmit={onSubmit}>
+      {notification.isOpen &&
+        <Notification type={notification.type} message={notification.message} onClose={() => {
+          setNotification(notiDetail)
+        }} />
+      }
       <Breadcrumb parent="Sản phẩm" pageName="Thông tin chi tiết sản phẩm" />
       <div className="relative bg-white sm:mb-[calc(20vh)] sm:rounded-lg sm:shadow-lg ">
         {loading &&
@@ -102,7 +149,32 @@ const ProductDetailPage = () => {
         <div className="p-5 flex flex-col sm:flex-row">
           <div className="md:max-w-[280px] sm:max-w-full">
             <div className="bg-gray-800 mb-3 text-lg font-semibold">Hình ảnh sản phẩm:</div>
-            <img src={productDetail ? productDetail.image : ''} alt="image" className="w-[260px] h-[360px]" />
+            <img src={urlFile || productDetail?.image} alt="image" className="w-[260px] h-[360px]" />
+            <Item
+              title="Cập nhập hình ảnh"
+              value={<div className="xsm:bottom-4 xsm:right-4">
+                <label
+                  htmlFor="cover"
+                  className="flex cursor-pointer items-center justify-center gap-2 rounded bg-primary py-1 px-2 text-sm font-medium text-white hover:bg-opacity-80 xsm:px-4"
+                >
+                  <input
+                    type="file"
+                    name="cover"
+                    id="cover"
+                    className="sr-only"
+                    accept="image/png, image/gif, image/jpeg"
+                    onChange={(e: any) => {
+                      e.preventDefault();
+                      const file = e?.target?.files[0];
+                      uploadAvatarUser(file)
+                    }} />
+                  <span>
+                    <img src="/admin/images/user/ic-camera.svg" alt="ic_camera" className="fill-current" />
+                  </span>
+                  <span>Upload</span>
+                </label>
+              </div>}
+            />
           </div>
           <div className="rounded-sm mb-5 md:p-10 grid sm:p-1 mt-4 sm:mt-0">
             <Item title="Mã sản phẩm" value={productDetail ? productDetail.id : '-'} />
@@ -110,7 +182,6 @@ const ProductDetailPage = () => {
             <Item title="Thời gian tạo" value={productDetail ? moment(productDetail.createdAt).format('DD/MM/YYYY') : '-'} />
             <Item title="Đánh giá sản phẩm" value={productDetail ? productDetail.rating : '-'} />
             <Item title="Lượt xem" value={productDetail ? productDetail.viewCount : '-'} />
-
             <Item
               title="Giá sản phẩm"
               value={<div className="relative">
